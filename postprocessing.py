4f175c9ac688c5fd5ad12b23b3f50a48cb1934c3
jkohler-u <117047665+jkohler-u@users.noreply.github.com>
2023-03-26 00:14:54 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -20,23 +20,14 @@ def convert_to_audio(data, noisy):
   #reverse the nromalisation
   S = data*40-40 +20
   #get the phase information
-  p = get_phase(noisy)
+  spectrogram = librosa.stft(data, n_fft=2048, hop_length=256, window='hann')
+  phase = np.angle(spectrogram)
   #convert the mel-spectrogram to a stft
   mag = librosa.feature.inverse.mel_to_stft(data, sr=16000)
   #convert the magnitude spectrogram into a complex spectorgram
-  spectrogram_complex = mag * np.exp(1j * np.angle(p[9]))
+  spectrogram_complex = mag * np.exp(1j * np.angle(phase))
   #convert the complex spectrogram to audio
-  reconstructed_audio=librosa.istft(spectrogram_complex ,hop_length=hop_length)
+  reconstructed_audio=librosa.istft(spectrogram_complex ,hop_length=256)
   #returns the audio data
   return reconstructed_audio
   
-def get_phase(data):
-  ''' get phase information from data
-  
-    Keyword arguments:
-    dataset -- the dataset to be sampled from
-  '''
- 
-  spectrogram = librosa.stft(data, n_fft=2048, hop_length=256, window='hann')
-  phase.append(np.angle(spectrogram))
-  return np.array(phase)
