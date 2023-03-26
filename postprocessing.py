e9885af86f8259302870db7b079c5c023395ed3b
m-schmie <117156837+m-schmie@users.noreply.github.com>
2023-03-26 14:35:11 +0200

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -12,22 +12,22 @@ import numpy as np
 import librosa
 
 def convert_to_audio(data, noisy):
-  ''' reconvert prediction to audio
+    ''' reconvert prediction to audio
   
-    Keyword arguments:
-    dataset -- the dataset to be sampled from
-  '''
-  #reverse the normalisation
-  data = data*40-40 +20
-  #get the phase information
-  spectrogram = librosa.stft(noisy, n_fft=2048, hop_length=256, window='hann')
-  phase = np.angle(spectrogram)
-  #convert the mel-spectrogram to a stft
-  mag = librosa.feature.inverse.mel_to_stft(data, sr=16000)
-  #convert the magnitude spectrogram into a complex spectorgram
-  spectrogram_complex = mag * np.exp(1j * np.angle(phase))
-  #convert the complex spectrogram to audio
-  reconstructed_audio=librosa.istft(spectrogram_complex ,hop_length=256)
-  #returns the audio data
-  return reconstructed_audio
+      Keyword arguments:
+      dataset -- the dataset to be sampled from
+    '''
+    #reverse the normalisation
+    data = data*40-40 +20
+    #get the phase information
+    spectrogram = librosa.stft(noisy, n_fft=2048, hop_length=256, window='hann')
+    phase = np.angle(spectrogram)
+    #convert the mel-spectrogram to a stft
+    mag = librosa.feature.inverse.mel_to_stft(data, sr=16000)
+    #convert the magnitude spectrogram into a complex spectorgram
+    spectrogram_complex = mag * np.exp(1j * np.angle(phase))
+    #convert the complex spectrogram to audio
+    reconstructed_audio=librosa.istft(spectrogram_complex ,hop_length=256)
+    #returns the audio data
+    return reconstructed_audio
   
