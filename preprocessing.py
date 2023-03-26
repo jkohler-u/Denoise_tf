7a6969f990bf7a9da757973614cb1fef3e215531
rondopondo <125920985+rondopondo@users.noreply.github.com>
2023-03-26 16:53:48 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -21,10 +21,14 @@ from sklearn.model_selection import train_test_split
 
         
 def prepare_data(name_dataset):
-    """ load the data, convert it to a tensor
-    
-    Keyword arguments: 
-    name_dataset - name of the folder the dataset was loaded into
+    """Loads audio files from the given dataset folder and converts them to tensors.
+
+    Args:
+        name_dataset (str): Name of the folder that contains the dataset.
+
+    Returns:
+        List of numpy arrays, where each array represents the audio data of a file.
+
     """
     data = []
    
@@ -41,40 +45,60 @@ def prepare_data(name_dataset):
     return data
 
 def make_same(data, size):
-    ''' make sure all data the same length
-    
-    Keyword arguments: 
-    data = the dataset
-    size - length all the data is normed to
+    ''' ensures that all data have the same length by padding
+     
+     Args:
+        data (list of numpy arrays): A list of audio samples, where each sample is represented as a numpy array.
+        size (int): The target length of the audio samples after normalization.
+        
+    Returns:
+        A tensor of shape (len(data), size) representing the audio data, where each row corresponds to an audio sample.
+        
     '''
+
     result = np.zeros((len(data), size), dtype=data[0].dtype)
     for i, d in enumerate(data):
         result[i, :min(size, d.shape[0])] = d[:min(size, d.shape[0])]
     return tf.stack(result)
 
-def noise(data):
-    ''' adding Gaussian noise to data
+def noise(data, noise_factor=0.004):
+    ''' Adds random noise to the audio data.
+
+    Args:
+        data (list of numpy arrays): A list of audio samples, where each sample is represented as a numpy array.
+        noise_factor (float, optional): The factor controlling the amount of noise to add to the audio data. Default is 0.004.
+
+    Returns:
+        A list of numpy arrays, where each array represents the audio data with added noise.
     
-    Keyword arguments: 
-    data = the dataset
     '''
     temp_data = []
-    noise_factor = 0.004
+    noise_factor = noise_factor
     # Add noise to the audio
     for i in data:
         temp = i + noise_factor*np.random.normal(size=i.shape)
         temp_data.append(temp)
     return temp_data
 
-def spectrogram(data):
-    ''' convert to a melspectrogram, normalize data, and add a faux-color dimension
+def spectrogram(data, sr =16000, n_fft=1024, hop_length=256,n_mels = 128):
+    ''' Computes the Mel spectrogram of the audio data.
     
-    Keyword arguments: 
-    data = the dataset
+    Args:
+        data (list of numpy arrays): A list of audio samples, where each sample is represented as a numpy array.
+        sr (int, optional): The sample rate of the audio data. Default is 16000.
+        n_fft (int, optional): The number of FFT points. Default is 1024.
+        hop_length (int, optional): The number of samples between successive frames. Default is 256.
+        n_mels (int, optional): The number of Mel bands to generate. Default is 128.
+        
+    Returns:
+        A tensor of shape (len(data), n_mels, T, 1), where each element is a float representing the Mel spectrogram
+        of the corresponding audio sample. T is the number of time frames in the spectrogram, which is determined
+        by the length of the input audio.
+    
     '''    
     spec = []
     for i in range(len(data)):
-        spectrogram = librosa.feature.melspectrogram(y=np.array(data[i]), sr = 16000, n_fft=1024, hop_length=256,n_mels = 128)
+        spectrogram = librosa.feature.melspectrogram(y=np.array(data[i]), sr = sr, n_fft=n_fft, hop_length=hop_length,n_mels = n_mels)
         spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
         # Normalize pixel values to be between -1 and 1
         spectrogram = ((spectrogram+40) / 40) 
